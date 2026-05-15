47e5fc561a695c73e00649b71c5b0595b4e279ed
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-05-15 15:21:20 -0300

    feat: add primary button component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { PrimaryButton } from "./components/primary-button/primary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, PrimaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/primary-button/primary-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  templateUrl: './primary-button.html',
+  styleUrl: './primary-button.css',
+})
+export class PrimaryButton {}
