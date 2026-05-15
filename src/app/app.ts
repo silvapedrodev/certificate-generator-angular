8bf6ed66f8a77660a9aa9d8c0bfba27cc88b6ee1
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-05-15 15:40:32 -0300

    feat: add secondary button component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { PrimaryButton } from "./components/primary-button/primary-button";
+import { SecondaryButton } from "./components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/secondary-button/secondary-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-button',
+  imports: [],
+  templateUrl: './secondary-button.html',
+  styleUrl: './secondary-button.css',
+})
+export class SecondaryButton {}
