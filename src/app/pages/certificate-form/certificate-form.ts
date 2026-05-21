b74fe0730f7db76ca65fd18eecdf342b3fbff356
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-05-21 16:09:31 -0300

    feat(certificates): create certificate interface and form submit handling

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -3,6 +3,7 @@ import { PrimaryButton } from "../../components/primary-button/primary-button";
 import { SecondaryButton } from "../../components/secondary-button/secondary-button";
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Certificate } from '../../interfaces/certificate';
 
 @Component({
   selector: 'app-certificate-form',
@@ -11,24 +12,34 @@ import { CommonModule } from '@angular/common';
   styleUrl: './certificate-form.css',
 })
 export class CertificateForm {
-  name: string = ""
-  activity: string = ""
-  activities: string[] = []
+  activity: string = ''
+
+  certificate: Certificate = {
+    activities: [],
+    name: ''
+  };
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched
   }
 
   formValid() {
-    return this.activities.length > 0 && this.name.length > 0;
+    return this.certificate.activities.length > 0 && this.certificate.name.length > 0;
   }
 
   addActivity() {
-    this.activities.push(this.activity);
+    this.certificate.activities.push(this.activity);
     this.activity = '';
   }
 
   removeActivity(index: number) {
-    this.activities.splice(index, 1);
+    this.certificate.activities.splice(index, 1);
+  }
+
+  submit() {
+    if (!this.formValid()) {
+      return
+    }
+    console.log(this.certificate)
   }
 }
