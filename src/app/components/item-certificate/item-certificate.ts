653cc6dc23b7e97ce13369cf2120aa95970f43d8
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-05-22 15:18:43 -0300

    feat(certificates): implement certificate list rendering logic

--- src/app/components/item-certificate/item-certificate.ts
+++ src/app/components/item-certificate/item-certificate.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SecondaryButton } from "../secondary-button/secondary-button";
 import { Router } from '@angular/router';
 
@@ -9,11 +9,13 @@ import { Router } from '@angular/router';
   styleUrl: './item-certificate.css',
 })
 export class ItemCertificate {
-  id: string = "6"
+  @Input() StudentName: string = '';
+  @Input() issueDate: string = '';
+  @Input() id: string = '';
 
   constructor(private router: Router) { }
 
   redirectCertificate() {
-    this.router.navigate(['/certificados', 2])
+    this.router.navigate(['/certificados', this.id])
   }
 }
